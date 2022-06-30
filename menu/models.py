ad5e22a0e0215e99d85b1a7cea548b31aecfd528
goranaviani <goranaviani@gmail.com>
2022-06-30 18:58:22 +0200

    django crud

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
