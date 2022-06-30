5cc1b91018c739e3be98d068df6168fb0fc67908
goranaviani <goranaviani@gmail.com>
2022-06-30 19:00:37 +0200

    django crud

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from menu.models import Menu
 
+#Serializers allows us to convert complex Django complex data structures such as
+# querysets or model instances in Python native objects that can be easily converted JSON/XML
+# format.
+
+
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
